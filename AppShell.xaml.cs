a4d5cbf0e239afa8de849e26304634ceb42ba29f
Cristian Luna <cristianlunarosario08@gmail.com>
2026-04-10 16:09:48 -0400

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoFinal_Movil_BibliotecaPersonal
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
